d94b4510c14784a066c37cebe73595802e285061
ariyanhex <vairobiulvai9@gmail.com>
2026-01-17 13:17:47 +0600

    Update Help.js

--- Script/commands/Help.js
+++ Script/commands/Help.js
@@ -32,7 +32,7 @@ module.exports.languages = {
 ┣━━━━━━━━━━━━━━━━┫
 ┃ ⚙ Prefix: %8
 ┃ 🤖 Bot Name: %9
-┃ 👑 Owner: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔
+┃ 👑 Owner: 𝘼𝙍𝙄𝙔𝘼𝙉 𝙀𝙑𝘼𝙉
 ╰━━━━━━━━━━━━━━━━╯`,
         "helpList": "[ There are %1 commands. Use: \"%2help commandName\" to view more. ]",
         "user": "User",
@@ -43,10 +43,10 @@ module.exports.languages = {
 
 // 🔹 এখানে আপনার ফটো Imgur লিংক করে বসাবেন ✅
 const helpImages = [
-    "https://i.imgur.com/sxSn1K3.jpeg",
-    "https://i.imgur.com/8WvpgUL.jpeg",
-    "https://i.imgur.com/8WvpgUL.jpeg",
-    "https://i.imgur.com/sxSn1K3.jpeg"
+    "https://i.imgur.com/Sa8hMB6.jpeg",
+    "https://i.imgur.com/X9BplLo.jpeg",
+    "https://i.imgur.com/ZsR6awC.jpeg",
+    "https://i.imgur.com/EOkK1vD.jpeg"
 ];
 
 
@@ -80,7 +80,7 @@ module.exports.handleEvent = function ({ api, event, getText }) {
         command.config.commandCategory || "Unknown",  
         command.config.cooldowns || 0,  
         prefix,  
-        global.config.BOTNAME || "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭"  
+        global.config.BOTNAME || "𝘼𝙍𝙄𝙔𝘼𝙉 𝘾𝙃𝘼𝙏 𝘽𝙊𝙏"  
     );  
 
     downloadImages(files => {  
@@ -110,7 +110,7 @@ module.exports.run = function ({ api, event, args, getText }) {
             command.config.commandCategory || "Unknown",  
             command.config.cooldowns || 0,  
             prefix,  
-            global.config.BOTNAME || "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭"  
+            global.config.BOTNAME || "𝘼𝙍𝙄𝙔𝘼𝙉 𝘾𝙃𝘼𝙏 𝘽𝙊𝙏"  
         );  
 
         downloadImages(files => {  
@@ -143,8 +143,8 @@ module.exports.run = function ({ api, event, args, getText }) {
 ${msg}
 ┣━━━━━━━━━━━━━━━━┫
 ┃ ⚙ Prefix: ${prefix}
-┃ 🤖 Bot Name: ${global.config.BOTNAME || "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭"}
-┃ 👑 Owner: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔
+┃ 🤖 Bot Name: ${global.config.BOTNAME || "𝘼𝙍𝙄𝙔𝘼𝙉 𝘾𝙃𝘼𝙏 𝘽𝙊𝙏"}
+┃ 👑 Owner: 𝘼𝙍𝙄𝙔𝘼𝙉 𝙀𝙑𝘼𝙉
 ╰━━━━━━━━━━━━━━━━╯`;
 
     downloadImages(files => {  
