9eebb586c3828f1b0bffcc66dd8352ada05412ce
ariyanhex <vairobiulvai9@gmail.com>
2026-01-17 13:23:41 +0600

    Update Helpall.js

--- Script/commands/Helpall.js
+++ Script/commands/Helpall.js
@@ -29,8 +29,8 @@ module.exports.run = async function ({ api, event }) {
  const finalText = `╔═══❖ 🌟 𝐂𝐎𝐌𝐌𝐀𝐍𝐃 𝐋𝐈𝐒𝐓 🌟 ❖═══╗
 ${allCommands.map(cmd => `║ ➔ ${cmd}`).join("\n")}
 ╠═════🔰 𝐁𝐎𝐓 𝐈𝐍𝐅𝐎 🔰═════╣
-║ 🤖 𝐁𝐨𝐭: ─꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭
-║ 👑 𝐎𝐰𝐧𝐞𝐫: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔
+║ 🤖 𝐁𝐨𝐭: ─꯭─⃝‌‌𝘾𝙔𝘽𝙀𝙍 𝘼𝙍𝙄𝙔𝘼𝙉 𝘾𝙃𝘼𝙏 𝘽𝙊𝙏
+║ 👑 𝐎𝐰𝐧𝐞𝐫: 𝘼𝙍𝙄𝙔𝘼𝙉 𝙀𝙑𝘼𝙉
 ║ 📦 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬: ${allCommands.length} 
 ╚═══════════════════════╝`;
 
@@ -38,8 +38,8 @@ ${allCommands.map(cmd => `║ ➔ ${cmd}`).join("\n")}
  const backgrounds = [
  "https://i.imgur.com/wu0iDqS.jpeg",
  "https://i.imgur.com/zqsuJnX.jpeg",
- "https://i.imgur.com/Huz3nAE.png",
- "https://i.imgur.com/wu0iDqS.jpeg"
+ "https://i.imgur.com/Sa8hMB6.jpeg",
+ "https://i.imgur.com/EOkK1vD.jpeg"
  ];
  const selectedBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
  const imgPath = __dirname + "/cache/helpallbg.jpg";
